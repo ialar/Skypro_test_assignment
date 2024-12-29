eabc99f26a6100f3e74fab1d214892a129ad58dc
Igor Larionov <jedialar@gmail.com>
2024-12-29 20:03:07 +0300

    black

--- chain/apps.py
+++ chain/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class ChainConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'chain'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "chain"
